09144e9f3c7ff26932067400ee9c2b11c82c412b
Ricardo M. <rmoctezumaf@gmail.com>
2025-10-17 20:20:21 -0500

    Added user input for xfade mode

--- xfade-looper.py
+++ xfade-looper.py
@@ -8,31 +8,24 @@ audio, fs = sf.read(file)
 
 fade_length_samples = int(fs * fade_length_sec)
 
-fade = np.linspace(0.0, 1.0, fade_length_samples)
+valid_response = False
+while valid_response == False:
+    mode = input('Linear or equal power xfade? (enter L or EP): ')
+    if mode == 'L':
+        fade = np.linspace(0.0, 1.0, fade_length_samples)
+        valid_response = True
+    elif mode == 'EP':
+        equal_power = lambda x: np.sqrt((1/2) * (1 + x))
+        fade = np.apply_along_axis(equal_power, 0, np.linspace(-1.0, 1.0, fade_length_samples))
+        valid_response = True
+    else:
+        print("Please enter L for linear or EP for equal power")
 
 fade_in = audio[:fade_length_samples] * fade
-fade_out = audio[-fade_length_samples:] * np.flip(fade.copy())
-
-xfade = fade_out + fade_in
-
-out = audio[fade_length_samples:-fade_length_samples]
-out = np.append(out, xfade)
-
-sf.write('xfade-linear.wav', out, fs)
-
-# Equal Power
-
-fade = np.linspace(-1.0, 1.0, fade_length_samples)
-
-equal_power_fade_in = lambda x: np.sqrt((1/2) * (1 + x))
-
-fade_in = audio[:fade_length_samples] * np.apply_along_axis(equal_power_fade_in, 0, fade)
-fade_out = audio[-fade_length_samples:] * np.apply_along_axis(equal_power_fade_in, 0, np.flip(fade))
-
+fade_out = audio[-fade_length_samples:] * np.flip(fade)
 xfade = fade_in + fade_out
 
 out = audio[fade_length_samples:-fade_length_samples]
 out = np.append(out, xfade)
 
-sf.write('xfade-equalpower.wav', out, fs)
-
+sf.write('xfade.wav', out, fs)
