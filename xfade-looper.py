57b5ac3a2c7f052d7741a52499c87cfbbc247e79
Ricardo M. <rmoctezumaf@gmail.com>
2025-10-17 20:43:45 -0500

    Added recursive output & user input for fade length

--- xfade-looper.py
+++ xfade-looper.py
@@ -1,31 +1,40 @@
 import soundfile as sf
 import numpy as np
+import os
+from glob import glob
 
-file = 'sweep_mono.wav'
-fade_length_sec = 1.0
+equal_power = lambda x: np.sqrt((1/2) * (1 + x))
 
-audio, fs = sf.read(file)
-
-fade_length_samples = int(fs * fade_length_sec)
+fade_length_sec = float(input('Please enter the desired crossfade length in seconds as a decimal number or integer: '))
 
 valid_response = False
 while valid_response == False:
     mode = input('Linear or equal power xfade? (enter L or EP): ')
+    if mode not in ['L', 'EP']:
+        print("Please enter L for linear or EP for equal power")
+    else:
+        valid_response = True
+
+if not os.path.exists('Crossfade Output'):
+    os.makedirs('Crossfade Output')
+
+for file in glob('*.wav'):
+    audio, fs = sf.read(file)
+
+    fade_length_samples = int(fs * fade_length_sec)
+
     if mode == 'L':
         fade = np.linspace(0.0, 1.0, fade_length_samples)
-        valid_response = True
     elif mode == 'EP':
-        equal_power = lambda x: np.sqrt((1/2) * (1 + x))
         fade = np.apply_along_axis(equal_power, 0, np.linspace(-1.0, 1.0, fade_length_samples))
-        valid_response = True
-    else:
-        print("Please enter L for linear or EP for equal power")
 
-fade_in = audio[:fade_length_samples] * fade
-fade_out = audio[-fade_length_samples:] * np.flip(fade)
-xfade = fade_in + fade_out
+    fade_in = audio[:fade_length_samples] * fade
+    fade_out = audio[-fade_length_samples:] * np.flip(fade)
+    xfade = fade_in + fade_out
 
-out = audio[fade_length_samples:-fade_length_samples]
-out = np.append(out, xfade)
+    out = audio[fade_length_samples:-fade_length_samples]
+    out = np.append(out, xfade)
 
-sf.write('xfade.wav', out, fs)
+    sf.write(f'Crossfade Output/xfade-{file}', out, fs)
+
+print('Saved files to /Crossfade Output')
