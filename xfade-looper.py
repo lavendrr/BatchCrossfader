a28e373e32666637f5c888ae776786cf2122b00a
Ricardo M. <rmoctezumaf@gmail.com>
2025-10-17 20:05:47 -0500

    Added equal power crossfade

--- xfade-looper.py
+++ xfade-looper.py
@@ -18,4 +18,21 @@ xfade = fade_out + fade_in
 out = audio[fade_length_samples:-fade_length_samples]
 out = np.append(out, xfade)
 
-sf.write('xfade.wav', out, fs)
+sf.write('xfade-linear.wav', out, fs)
+
+# Equal Power
+
+fade = np.linspace(-1.0, 1.0, fade_length_samples)
+
+equal_power_fade_in = lambda x: np.sqrt((1/2) * (1 + x))
+
+fade_in = audio[:fade_length_samples] * np.apply_along_axis(equal_power_fade_in, 0, fade)
+fade_out = audio[-fade_length_samples:] * np.apply_along_axis(equal_power_fade_in, 0, np.flip(fade))
+
+xfade = fade_in + fade_out
+
+out = audio[fade_length_samples:-fade_length_samples]
+out = np.append(out, xfade)
+
+sf.write('xfade-equalpower.wav', out, fs)
+
